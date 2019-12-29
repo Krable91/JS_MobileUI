54ac81756ba3ffdcdf3d09d0aa545175121df1f3
krable91 <dydansp@naver.com>
2019-12-29 20:14:25 +0900

    191225

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-const countMinus = document.querySelector("#Cart_Count_Minus");
-const countPlus = document.querySelector("#Cart_Count_Plus");
 const countContainer = document.querySelector("#productCount");
-let countChange = countContainer.innerText;
+const countPlus = document.querySelector("#Cart_Count_Plus");
+const countMinus = document.querySelector("#Cart_Count_Minus");
+const colorClear = document.querySelector(".Option_Color_Clear");
+const colockContainer = document.querySelector(".Header_Time");
+const clockTitle = document.querySelector("#clockTitle");
+
+let count = 0;
 
 function minusClicked(){
-    countContainer.innerText = countChange--;
+    if ( count === 0 ){
+        alert("제품 개수가 0개입니다.");
+    } else {
+    count --;
+    countContainer.innerText = count;
+    }
 }
 function plusClicked(){
-    countContainer.innerText = countChange++;
+    count ++;
+    countContainer.innerText = count;
 }
+function colorReset(){
 
+}
+function getTime(){
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
 
+}
 function init() {
-
     countMinus.addEventListener("click", minusClicked);
     countPlus.addEventListener("click", plusClicked);
+    colorClear.addEventListener("click", colorReset);
+    getTime();
+    setInterval(getTime, 1000);
 
 }
-init();
+
+init()
