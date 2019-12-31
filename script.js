8dfa3a51e8c6d6f8f56c2cc966124ec88f81ccdf
krable91 <dydansp@naver.com>
2019-12-31 23:07:03 +0900

    Size Select

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ const countMinus = document.querySelector("#Cart_Count_Minus");
 const colorClear = document.querySelector(".Option_Color_Clear");
 const colockContainer = document.querySelector(".Header_Time");
 const clockTitle = document.querySelector("#clockTitle");
+const starButton = document.getElementById("Content_Top_Star");
+const sizeButton = document.getElementsByClassName("size")
+const likeButton = document.getElementById("Like");
+const sizeClear = document.getElementById("Option_Size_Clear");
 
 let count = 0;
+let heartFull = false;
+let starFull = false;
+let sizeChecked = false;
+
+Array.from(sizeButton).forEach(item => item.addEventListener("click", sizeSelect));
 
 function minusClicked(){
     if ( count === 0 ){
@@ -30,13 +39,46 @@ function getTime(){
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
 
 }
+function star(){
+    if(starFull === false){
+        starButton.innerHTML = `4.3<i class="fas fa-star"></i>`;
+        starFull = true;
+    } else {
+        starButton.innerHTML = `4.3<i class="far fa-star"></i>`;
+        starFull = false;
+    }
+}
+function delSize(){
+
+}
+function sizeSelect(){
+    const text = event.target.innerText;
+        if(event.target.value === "false"){
+            event.target.value = "true";
+            event.target.innerHTML = `<i class="fas fa-check"></i>` + text;
+        } else {
+            event.target.value = "false";
+            event.target.innerText = text;
+        }
+}
+function like(){
+    if(heartFull === false){
+        likeButton.innerHTML = `<i class="fas fa-heart"></i>`;
+        heartFull = true;
+    } else {
+        likeButton.innerHTML = `<i class="far fa-heart"></i>`;
+        heartFull = false;
+    }
+}
 function init() {
     countMinus.addEventListener("click", minusClicked);
     countPlus.addEventListener("click", plusClicked);
     colorClear.addEventListener("click", colorReset);
+    likeButton.addEventListener("click", like);
+    starButton.addEventListener("click", star);
+    sizeClear.addEventListener("click", delSize);
     getTime();
     setInterval(getTime, 1000);
-
 }
 
 init()
